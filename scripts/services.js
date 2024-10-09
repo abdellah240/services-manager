76c7eef8c5aa72f67704bbbf9c0bc0889550e334
abdellah240 <155846993+abdellah240@users.noreply.github.com>
2024-10-08 20:28:08 -0400

    Modify services, add comments

--- scripts/services.js
+++ scripts/services.js
@@ -1,30 +1,37 @@
-class Service{
-    constructor(name,description,price){
-        this.name=name;
-        this.description=description;
-        this.price=price;
-    }
-
-}
+// Declare array of services
 const servicesArray = [];
-let callConstructor = () => {
-    document.getElementById("services-list").innerHTML= "";
 
-    addedService = new Service (document.getElementById("name").value, 
-                                document.getElementById("description").value, 
-                                document.getElementById("price").value);
-    servicesArray.push(addedService);
+// Wait for click, then execute anonymous function
+document.getElementById("confirm-service-button").addEventListener("click", () =>
+{
+    // Clear previous list
+    document.getElementById("services-list-div").innerHTML = "";
 
-    for(i=0; i<servicesArray.length; i++){
+    // Create new service object 
+    const newService = {
+        title: document.getElementById("title").value,
+        description: document.getElementById("description").value,
+        price: document.getElementById("price").value
+    };
+
+    servicesArray.push(newService);
+
+    // For each service, execute anonymous function
+    servicesArray.forEach(() =>
+    {
+        // Formatting every service
         var serviceDiv = document.createElement("div");
         serviceDiv.className = "service";
+        serviceDiv.innerHTML = `Title: ${newService.title} <br>
+                                Description: ${newService.description} <br>
+                                Price ${newService.price}`;
 
-        serviceDiv.innerHTML = `Name: ${servicesArray[i].name} <br>
-                                Description: ${servicesArray[i].description} <br>
-                                Price ${servicesArray[i].price}`;
-
-        document.getElementById("services-list").appendChild(serviceDiv);
-    }
-    document.getElementById("service-form").reset();
-}
+        // Adding every service to the updated list
+        document.getElementById("services-list-div").appendChild(serviceDiv);
+    })
+    // Clear textboxes
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("price").value = "";
+})
 
