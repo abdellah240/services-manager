e40d0ecfb13bb55d4c5f69321140ca5ecca07911
paul-sans-l <163008790+paul-sans-l@users.noreply.github.com>
2024-10-15 13:47:56 -0400

    bills,home, main,services

--- administrator.js
+++ administrator.js
@@ -1,16 +1,16 @@
-// var ID = "paul@hoomail.com";
-// var pass = "soen287";
-// var isSigned = false;
+var ID = "paul@hoomail.com";
+var pass = "soen287";
 
-// function checker(){
-//   var inputId = document.getElementById("Email-administrator").value;
-//   var inputPass = document.getElementById("Password-administrator").value;
 
-//   if(inputId == ID && inputPass == pass)
-//     alert("Correct!!");
-//   else {
-//      alert("Incorrect");}
+function checker(){
+  var inputId = document.getElementById("Email-administrator").value;
+  var inputPass = document.getElementById("Password-administrator").value;
 
-// }
+  if(inputId == ID && inputPass == pass)
+    alert("Correct!!");
+  else {
+     alert("Incorrect");}
+
+}
 
   
