4d009e0bd7048d220267d877f40ee8ac53e4a45a
abdellah240 <155846993+abdellah240@users.noreply.github.com>
2024-10-18 21:17:41 -0400

    Add edit services

--- scripts/services.js
+++ scripts/services.js
@@ -1,12 +1,12 @@
 // Declare Array of services
 const servicesArray = [];
 
-displayServices();
+let isEditing = false;
+let indexOfEdit = null;
 
-// Wait for click, then execute anonymous function
+// On click, create or edit service object 
 document.getElementById("confirm-service-button").addEventListener("click", () =>
 {
-    // Create new service object 
     const newService = (
         {
             title: document.getElementById("title").value,
@@ -14,35 +14,54 @@ document.getElementById("confirm-service-button").addEventListener("click", () =
             price: document.getElementById("price").value
         }
     )
-    servicesArray.push(newService)
+
+    if (isEditing === false)
+        servicesArray.push(newService);
+
+    if (isEditing === true)
+    {
+        servicesArray[indexOfEdit] = newService;
+        
+        // Reset state and index after editing
+        isEditing = false;
+        indexOfEdit = null;
+    }
+    
     displayServices();
 });
 
 function displayServices()
 {
-
     const servicesListDiv = document.getElementById("services-list-div");
-    servicesListDiv.innerHTML = ""
+    servicesListDiv.innerHTML = "";
 
     servicesArray.forEach((service, index) =>
     {
-        // Set format and buttons for each service
-
         const serviceDiv = document.createElement("div");
+
+        // Set text and buttons for each service
         const deleteServiceButton = document.createElement("button");
-
-        deleteServiceButton.innerHTML = 'Delete';
-        serviceDiv.className = "service";
-        deleteServiceButton.className = "delete-service";
-
-        // When clicked, delete service 
-        deleteServiceButton.addEventListener("click", () => deleteService(index));
+        const editServiceButton = document.createElement("button");
 
         serviceDiv.innerHTML = `Title: ${service.title} <br>
-                                Description: ${service.description} <br>
-                                Price: ${service.price}`;
-        serviceDiv.appendChild(deleteServiceButton);
+                                    Description: ${service.description} <br>
+                                    Price: ${service.price}`;
 
+        deleteServiceButton.innerHTML = 'Delete';
+        editServiceButton.innerHTML = 'Edit';
+
+        // Set classnames
+        serviceDiv.className = "service";
+        deleteServiceButton.className = "delete-service";
+        editServiceButton.className = "edit-service";
+
+        // When clicked, delete or edit service 
+        deleteServiceButton.addEventListener("click", () => deleteService(index));
+        editServiceButton.addEventListener("click", () => editService(index));
+
+        serviceDiv.appendChild(deleteServiceButton);
+        serviceDiv.appendChild(editServiceButton);
+        ;
         // Add each service to the service list
         servicesListDiv.appendChild(serviceDiv);
     });
@@ -53,8 +72,21 @@ function displayServices()
     document.getElementById("price").value = "";
 }
 
+
 function deleteService(index)
 {
     servicesArray.splice(index, 1);
     displayServices();
+}
+
+function editService(index)
+{
+    // Set state and store index
+    isEditing = true;
+    indexOfEdit = index;
+    
+    // Fill textboxes with service information
+    document.getElementById("title").value = servicesArray[index].title;
+    document.getElementById("description").value = servicesArray[index].description;
+    document.getElementById("price").value = servicesArray[index].price;
 }
