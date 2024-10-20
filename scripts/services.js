11390d255de106ade3fb7b4a19ad1a8a23052e2e
Ryan Malaeb <ryanmalaeb4@gmail.com>
2024-10-20 14:26:02 +0300

    update styles

--- scripts/services.js
+++ scripts/services.js
@@ -1,37 +1,88 @@
 // Declare Array of services
 const servicesArray = [];
 
-// Wait for click, then execute anonymous function
-document.getElementById("confirm-service-button").addEventListener("click", () =>
-{
-    // Clear previous list
-    document.getElementById("services-list-div").innerHTML = "";
+let isEditing = false;
+let indexOfEdit = null;
 
-    // Create new service object 
+// On click, create or edit service object
+document
+  .getElementById("confirm-service-button")
+  .addEventListener("click", () => {
     const newService = {
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
-        price: document.getElementById("price").value
+      title: document.getElementById("title").value,
+      description: document.getElementById("description").value,
+      price: document.getElementById("price").value,
     };
 
-    servicesArray.push(newService);
+    if (isEditing === false) servicesArray.push(newService);
 
-    // For each service, execute anonymous function
-    servicesArray.forEach(() =>
-    {
-        // Formatting every service
-        var serviceDiv = document.createElement("div");
-        serviceDiv.className = "service";
-        serviceDiv.innerHTML = `Title: ${newService.title} <br>
-                                Description: ${newService.description} <br>
-                                Price ${newService.price}`;
+    if (isEditing === true) {
+      servicesArray[indexOfEdit] = newService;
 
-        // Adding every service to the updated list
-        document.getElementById("services-list-div").appendChild(serviceDiv);
-    })
-    // Clear textboxes
-    document.getElementById("title").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("price").value = "";
-})
+      // Reset state and index after editing
+      isEditing = false;
+      indexOfEdit = null;
+    }
 
+    displayServices();
+  });
+
+function displayServices() {
+  const servicesListDiv = document.getElementById("services-list-div");
+  servicesListDiv.innerHTML = "";
+
+  servicesArray.forEach((service, index) => {
+    const serviceDiv = document.createElement("div");
+    const buttonContainerDiv = document.createElement("div");
+
+    // Set text and buttons for each service
+    const deleteServiceButton = document.createElement("button");
+    const editServiceButton = document.createElement("button");
+    serviceDiv.appendChild(buttonContainerDiv);
+
+    serviceDiv.innerHTML = `Title: ${service.title} <br>
+                                    Description: ${service.description} <br>
+                                    Price: ${service.price}`;
+
+    deleteServiceButton.innerHTML = "Delete";
+    editServiceButton.innerHTML = "Edit";
+
+    // Set classnames
+    serviceDiv.className = "services__service";
+    deleteServiceButton.className = "button";
+    editServiceButton.className = "button";
+    buttonContainerDiv.className = "services__button-container";
+
+    // When clicked, delete or edit service
+    deleteServiceButton.addEventListener("click", () => deleteService(index));
+    editServiceButton.addEventListener("click", () => editService(index));
+
+    serviceDiv.appendChild(buttonContainerDiv);
+    buttonContainerDiv.appendChild(deleteServiceButton);
+    buttonContainerDiv.appendChild(editServiceButton);
+    // Add each service to the service list
+    servicesListDiv.appendChild(serviceDiv);
+  });
+
+  // Clear textboxes
+  document.getElementById("title").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("price").value = "";
+}
+
+function deleteService(index) {
+  servicesArray.splice(index, 1);
+  displayServices();
+}
+
+function editService(index) {
+  // Set state and store index
+  isEditing = true;
+  indexOfEdit = index;
+
+  // Fill textboxes with service information
+  document.getElementById("title").value = servicesArray[index].title;
+  document.getElementById("description").value =
+    servicesArray[index].description;
+  document.getElementById("price").value = servicesArray[index].price;
+}
