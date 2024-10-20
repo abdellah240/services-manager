30b2d5ab944f7e83ab12250f70945d3b4c66f002
paul-sans-l <163008790+paul-sans-l@users.noreply.github.com>
2024-10-20 13:34:16 -0400

    Admin option, login file and customer main page

--- scripts/administrator.js
+++ scripts/administrator.js
@@ -1,16 +1,36 @@
-var ID = "paul@hoomail.com";
-var pass = "soen287";
+let ID = "paul@hoomail.com";
+let pass = "soen287";
 
 
 function checker(){
+
   var inputId = document.getElementById("Email-administrator").value;
   var inputPass = document.getElementById("Password-administrator").value;
+  
+    if(inputId == ID && inputPass == pass){
+      localStorage.setItem("isSigned1", "true");
+      window.location.href = "Manage-Business.html";
+  }
+    else {
+      isSigned1 =false;
+      alert("Incorrect");
+      window.location.href = "login-administrator.html";
+      }
+}
 
-  if(inputId == ID && inputPass == pass)
-    alert("Correct!!");
-  else {
-     alert("Incorrect");}
 
+function logout() {
+  var confirmation = confirm("Are you sure you want to log out?");
+  
+  // Check if the user clicked "OK"
+  if (confirmation) {
+      // If confirmed, log the user out (clear sign-in status)
+      localStorage.setItem("isSigned1", "false");
+      window.location.href = "index.html"; // Redirect to login page
+  } else {
+      // If canceled, do nothing (stay on the current page)
+      console.log("User canceled logout.");
+  }
 }
 
   
