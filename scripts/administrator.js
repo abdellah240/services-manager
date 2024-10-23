683a20db6e989950dc86254824f0b52b6dc8acea
paul-sans-l <163008790+paul-sans-l@users.noreply.github.com>
2024-10-23 17:13:29 -0400

    Admin features

--- scripts/administrator.js
+++ scripts/administrator.js
@@ -33,4 +33,76 @@ function logout() {
   }
 }
 
-  
+function logoclicked(){
+  document.getElementById("showButton").addEventListener("click", function() {
+    const content = document.getElementById("hiddenContent");
+    const content2 = document.getElementById("hidden");
+    const content3 = document.getElementById("hidden2");
+    if (content.style.display === "none") {
+      content.style.display = "block"; // Show the content
+      content2.style.display = "none";
+      content3.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content3.style.display = "none";
+      content.style.display = "none"; // Hide the content if clicked again
+    }
+  });
+}
+function Appclicked(){
+  document.getElementById("showButton1").addEventListener("click", function() {
+    const content = document.getElementById("hidden");
+    const content2 = document.getElementById("hiddenContent");
+    const content3 = document.getElementById("hidden2");
+    if (content.style.display === "none") {
+      content.style.display = "block"; // Show the content
+      content2.style.display = "none";
+      content3.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content3.style.display = "none";
+      content.style.display = "none"; // Hide the content if clicked again
+    }
+  });
+}
+function Homeclicked(){
+  document.getElementById("showButton2").addEventListener("click", function() {
+    const content = document.getElementById("hidden2");
+    const content2 = document.getElementById("hiddenContent");
+    const content3 = document.getElementById("hidden");
+    if (content.style.display === "none") {
+      content.style.display = "block"; // Show the content
+      content2.style.display = "none";
+      content3.style.display = "none";
+
+    } else {
+      content2.style.display = "none";
+      content3.style.display = "none";
+      content.style.display = "none"; // Hide the content if clicked again
+    }
+  });
+}
+
+function saveChanges(){
+  const logo = document.getElementById("logo");
+  const saveButton = document.getElementById("saveButton");
+  const input = document.getElementById("input");
+  saveButton.addEventListener('click', function() {
+    const selectedFile = input.files[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+        
+        reader.onload = function(e) {
+          logo.src = e.target.result; 
+        };
+
+        reader.readAsDataURL(selectedFile);
+    } else {
+      // No file is selected
+      messageDiv.innerHTML = "Please select a file before saving changes.";
+      messageDiv.style.color = "red";
+    }
+  });
+    }
+
