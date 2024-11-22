b159114709bddfac135aad51cffc54d62108f526
abdellah240 <155846993+abdellah240@users.noreply.github.com>
2024-11-22 15:48:24 -0500

    commented out unfinished code

--- routes/account-signup/index.js
+++ routes/account-signup/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const getServices = require("./getServices"); 
-const addService = require("./addService");   
-const deleteService = require("./deleteService");
-const editService = require("./editService");
+//const express = require("express");
+//const getServices = require("./getServices"); 
+//const addService = require("./addService");   
+//const deleteService = require("./deleteService");
+//const editService = require("./editService");
 
 // Import db (connection)
-const db = require("../db2");  
+//const db = require("../db2");  
 
-const router = express.Router(); // Similar to "const app=express()". Does not start a server.
+//const router = express.Router(); // Similar to "const app=express()". Does not start a server.
     
 //Setup routes, pass db to functions
-router.get("/", getServices(db));
-router.post("/", addService(db));
-router.delete("/", deleteService(db));
-router.put('/', editService(db));
+//router.get("/", getServices(db));
+//router.post("/", addService(db));
+//router.delete("/", deleteService(db));
+//router.put('/', editService(db));
 
-module.exports = router;
+//module.exports = router;
