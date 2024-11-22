22f9feb31487fa07ada9f1dd88e0a1d820256d25
abdellah240 <155846993+abdellah240@users.noreply.github.com>
2024-11-22 18:21:31 -0500

    fixed

--- routes/account-signup/index.js
+++ routes/account-signup/index.js
@@ -4,9 +4,9 @@ const signUpInfo = require("./signUpInfo");
 // Import db (connection)
 const db2 = require("../db2");  
 
-//const router = express.Router(); // Similar to "const app=express()". Does not start a server.
+const router = express.Router(); // Similar to "const app=express()". Does not start a server.
     
 //Setup routes, pass db to functions
 router.post('/', signUpInfo(db2));
 
-//module.exports = router;
+module.exports = router;
