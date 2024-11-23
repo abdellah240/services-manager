8a5f368099ac268e47c4556def558f4cf6381d17
RyanMalaeb <ryanmalaeb4@gmail.com>
2024-11-23 12:54:34 -0500

    update main data base password

--- routes/main-db.js
+++ routes/main-db.js
@@ -4,7 +4,7 @@ const mainDatabase = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: 'secret',
+    password: '',
     database: 'services-manager'
 })
 
