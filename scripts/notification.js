a55262f6c5ad90896da64e60b47d1b20cb46d223
paul-sans-l <163008790+paul-sans-l@users.noreply.github.com>
2024-11-23 21:22:44 -0500

    Notification overwritting

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,7 +1,20 @@
 function acceptPayment(){
-
 }
 
 function dismissNotification(){
     
+}
+
+class Notification {
+    constructor(Title, content, date) {
+        this.Title = Title;
+        this.content = content;
+        this.date = date;
+    }
+}
+
+class Answers extends Notification{
+    constructor(content, date){
+        super("Answer from support", content,date);
+    }
 }
